216dca57ec822384d36afca70af270528540ff5a
Leonard Lee <leonardlee@Leonards-MacBook-Pro.local>
2018-05-18 02:35:50 -0700

    create a module folder for classes

---
+++ module/Thermostat.py
@@ -0,0 +1,27 @@
+class Thermostat(object):
+    def __init__(self, name):
+        # private variables
+        self._id = Unity.getID()
+        self._tmp = Unity.getTemp()
+
+        # public variables
+        self.name = name
+        # "cool", "heat", "off"
+        self.operation_mode = "off"
+        # 30 - 100 degrees fahrenheit
+        self.cool_set_point = 70
+        self.hot_set_point = 70
+        # "off", "auto"
+        self.fan_mode = "off"
+
+    def setCoolPoint(self, point):
+        if point > 100:
+            self.cool_set_point = 100;
+        elif point < 70:
+            self.cool_set_point = 70
+
+    def setHotPoint(self, point):
+        if point > 100:
+            self.hot_set_point = 100;
+        elif point < 70:
+            self.hot_set_point = 70
