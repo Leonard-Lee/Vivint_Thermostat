761207f846082e70f7cb12ff7b1000ff11d9fa31
Leonard Lee <leonardlee@Leonards-MacBook-Pro.local>
2018-05-20 02:01:39 -0700

    Change the two main methods in Unity to be class methods

--- module/Unity.py
+++ module/Unity.py
@@ -1,20 +1,15 @@
 import random
+import web
 
 class Unity(object):
-    def __init__(self):
-        # users could search the thermostat by name
-        self.dict_name_thermostat = {}
-        # users could search the thermostat by id
-        self.dict_id_thermostat = {}
-
-        self.last_id = 0
-
     # provide UUID for thermostat ID
-    def getLastID(self):
-        id = self.last_id
-        self.last_id += 1
+    @classmethod
+    def getLastID(cls):
+        id = web.last_id
+        web.last_id += 1
         return id
 
     # provide temperature degree(gF)
-    def getTemperature(self):
+    @classmethod
+    def getTemperature(cls):
         return random.randint(30, 120)
